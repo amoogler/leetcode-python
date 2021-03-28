0e006d61822ce51732b1681c51c9a145b13610ff
Eric Li <lishuokobe2@gmail.com>
2021-03-28 14:47:41 -0700

    Faster string concatenation for 1678.py.

--- 1678.py
+++ 1678.py
@@ -1,17 +1,17 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        final_string = ""
+        final = []
         
         if command[0] is 'G':
-            final_string += 'G'
+            final.append('G')
         
         for i in range(1, len(command)):
             if command[i] is 'G':
-                final_string += 'G'
+                final.append('G')
             elif command[i] is ')':
                 if command[i - 1] is '(':
-                    final_string += 'o'
+                    final.append('o')
                 elif command[i - 1] is 'l':
-                    final_string += 'al'
+                    final.append('al')
         
-        return final_string
+        return ''.join(final)
