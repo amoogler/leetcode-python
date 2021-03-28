9ca763be17f8390dde2356239dfa8146ffcc964e
Eric Li <lishuokobe2@gmail.com>
2021-03-27 20:42:02 -0700

    Check in leetcode 1800.py.

---
+++ 1800.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        slow = 0
+        subarray_sum = nums[slow]
+        max_sum = subarray_sum
+        
+        if len(nums) == 0:
+            return max_sum
+
+        for fast in range(1, len(nums)):
+            if nums[fast] > nums[fast - 1]:
+                subarray_sum += nums[fast]
+                max_sum = max(subarray_sum, max_sum)
+            else:
+                slow = fast
+                subarray_sum = nums[slow]
+                
+        return max_sum
+        
