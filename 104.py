1e4cbf8b6d727d77cc52eae663f9ee70c2bba4b9
Eric Li <lishuokobe2@gmail.com>
2021-03-30 20:59:49 -0700

    Correct 104.py.

--- 104.py
+++ 104.py
@@ -5,17 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
+    def maxDepth(self, root: TreeNode) -> int:
         if root == None:
             return 0
         
         if root.left == None and root.right == None:
             return 1
         
-        if root.left == None:
-            return self.minDepth(root.right) + 1
-        
-        if root.right == None:
-            return self.minDepth(root.left) + 1
-        
-        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
