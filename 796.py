444bd3621aa49ac0ca523d301fb3391383458262
Eric Li <lishuokobe2@gmail.com>
2021-03-31 17:24:04 -0700

    Improved 796.py.

--- 796.py
+++ 796.py
@@ -1,18 +1,3 @@
 class Solution:
     def rotateString(self, A: str, B: str) -> bool:
-        if len(A) != len(B):
-            return False
-        
-        if len(A) == 0 and len(B) == 0:
-            return True
-        
-        length = len(A)
-        
-        while length > 0:
-            length -= 1
-            A = A[1:len(A)] + A[0:1]
-            
-            if A == B:
-                return True
-        
-        return False
+        return len(A) == len(B) and B in A + A
