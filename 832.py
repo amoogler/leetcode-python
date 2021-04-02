32de59c4d483b89f7c4f1ac66cd3e1df578a18a2
Eric Li <lishuokobe2@gmail.com>
2021-04-01 17:39:57 -0700

    Improved 832.py.

--- 832.py
+++ 832.py
@@ -3,10 +3,10 @@ from typing import List
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
         # Flip the image horizontally.
-        image = [row[::-1] for row in image]
+        flipped_image = [row[::-1] for row in image]
         
         # Invert the image.
-        for idx, row in enumerate(image):
-            image[idx] = [pixel ^ 1 for pixel in row]
+        for idx, row in enumerate(flipped_image):
+            flipped_image[idx] = [pixel ^ 1 for pixel in row]
         
-        return image        
+        return flipped_image
