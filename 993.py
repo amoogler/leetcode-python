8d1a67c85a9332ceacb095b59c9e0a726ddea07e
Eric Li <lishuokobe2@gmail.com>
2021-04-03 12:00:10 -0700

    Check in 993.py.

---
+++ 993.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        queue = collections.deque([root])
+        
+        while queue:
+            level = []
+            level_length = len(queue)
+            
+            while level_length > 0:
+                node = queue.popleft()
+                level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                    
+                level_length -= 1
+                
+                if not node.left or not node.right:
+                    continue
+                
+                # Siblings
+                if (x == node.left.val and y == node.right.val) or \
+                   (x == node.right.val and y == node.left.val):
+                    return False
+
+            if x in level and y in level:
+                return True
+              
+        return False
