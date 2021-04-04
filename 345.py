e19a824e45b8caf0165989459485a0a900294fdf
Eric Li <lishuokobe2@gmail.com>
2021-04-04 12:53:10 -0700

    Improved 345.py.

--- 345.py
+++ 345.py
@@ -1,20 +1,18 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         start, end = 0, len(s) - 1
-        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        vowels = set("aeiouAEIOU")
         word = list(s)
         
         while start < end:
-            if word[start] in vowels and word[end] in vowels:
-                word[start], word[end] = word[end], word[start]
+            while start < end and word[start] not in vowels:
                 start += 1
+
+            while start < end and word[end] not in vowels:
                 end -= 1
-            elif word[start] in vowels:
-                end -= 1
-            elif word[end] in vowels:
-                start += 1
-            else:
-                start += 1
-                end -= 1
+
+            word[start], word[end] = word[end], word[start]
+            start += 1
+            end -= 1
         
         return ''.join(word)
