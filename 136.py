4b20a49f0d0f0abc65a8d49aeaf69eb97b2693e4
Eric Li <lishuokobe2@gmail.com>
2021-04-04 09:28:46 -0700

    Improved 136.py.

--- 136.py
+++ 136.py
@@ -2,9 +2,9 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        single = 0
+        single = nums[0]
 
-        for num in nums:
+        for num in nums[1:]:
             single ^= num
         
         return single
