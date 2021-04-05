fb1e0851aafa8218786aae4e730f80300c6087a4
Eric Li <lishuokobe2@gmail.com>
2021-04-05 08:32:46 -0700

    Improved 1365.py.

--- 1365.py
+++ 1365.py
@@ -1,17 +1,11 @@
+import bisect
+
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         sorted_nums = sorted(nums)
         ans = []
         
         for num in nums:
-            idx = sorted_nums.index(num)
-            
-            while idx > 0:
-                if num > sorted_nums[idx - 1]:
-                    break
-                else:
-                    idx -= 1
-
-            ans.append(idx)
+            ans.append(bisect.bisect_left(sorted_nums, num))
 
         return ans
