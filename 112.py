a636cf395c691c47545cb594349286bc9bc4b521
Eric Li <lishuokobe2@gmail.com>
2021-04-06 20:56:59 -0700

    Improved 112.py.

--- 112.py
+++ 112.py
@@ -4,7 +4,6 @@ class Solution:
         max_profit = 0
 
         for i in range(1, len(prices)):
-
             if prices[i - 1] < prices[i]:
                 max_profit = max(max_profit, prices[i] - min_value)
             else:
