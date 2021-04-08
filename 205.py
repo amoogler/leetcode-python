4564b9e8bef844833a6169696c7ac854cba04d34
Eric Li <lishuokobe2@gmail.com>
2021-04-08 08:52:40 -0700

    Improved 205.py.

--- 205.py
+++ 205.py
@@ -1,30 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        def helper(s, t):
+            d = {}
 
-        dict1, dict2 = dict(), dict()
+            for a, b in zip(s, t):
+                if a not in d:
+                    d[a] = b
+                else:
+                    if d[a] != b:
+                        return False
 
-        for s_char, t_char in zip(s, t):
-            if s_char in dict1:
-                if dict1[s_char] != t_char:
-                    return False
-            else:
-                dict1[s_char] = t_char
+            return True
 
-        for s_char, t_char in zip(s, t):
-            if t_char in dict2:
-                if dict2[t_char] != s_char:
-                    return False
-            else:
-                dict2[t_char] = s_char
-
-        for key, value in dict1.items():
-            if dict2[value] != key:
-                return False
-
-        for key, value in dict2.items():
-            if dict1[value] != key:
-                return False
-
-        return True
+        return helper(s, t) and helper(t, s)
