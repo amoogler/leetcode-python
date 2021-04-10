d05a631d139a2005010a41a48517d063ff5824a2
Eric Li <lishuokobe2@gmail.com>
2021-04-09 22:23:28 -0700

    Check in 94.py.

---
+++ 94.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursive
+# class Solution:
+#     def inorderTraversal(self, root: TreeNode) -> List[int]:
+#         inorder_nodes = []
+#         self.inorder(root, inorder_nodes)
+#         return inorder_nodes
+
+#     def inorder(self, node: TreeNode, inorder_nodes: List[int]):
+#         if not node:
+#             return
+
+#         self.inorder(node.left, inorder_nodes)
+#         inorder_nodes.append(node.val)
+#         self.inorder(node.right, inorder_nodes)
+
+# Iterative
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        inorder_nodes, stack = [], []
+        current = root
+
+        while len(stack) > 0 or current:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                node = stack.pop()
+                inorder_nodes.append(node.val)
+                current = node.right
+
+        return inorder_nodes
