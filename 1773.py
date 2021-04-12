f2e72243f59943f7f3bed512ae96600676fe723d
Eric Li <lishuokobe2@gmail.com>
2021-04-11 20:39:29 -0700

    Improved 1773.py.

--- 1773.py
+++ 1773.py
@@ -1,15 +1,10 @@
-def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-    idx, count = 0, 0
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        count = 0
+        d = {"type": 0, "color": 1, "name": 2}
 
-    if ruleKey == "type":
-        idx = 0
-    elif ruleKey == "color":
-        idx = 1
-    elif ruleKey == "name":
-        idx = 2
+        for item in items:
+            if item[d[ruleKey]] == ruleValue:
+                count += 1
 
-    for item in items:
-        if item[idx] == ruleValue:
-            count += 1
-
-    return count
+        return count
