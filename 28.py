78af6d913131fae433083ff2ddaa1422106139de
Eric Li <lishuokobe2@gmail.com>
2021-04-11 20:47:58 -0700

    Improved 28.py.

--- 28.py
+++ 28.py
@@ -1,11 +1,8 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-
         idx, hay_length, nee_length = 0, len(haystack), len(needle)
 
-        while idx < hay_length and hay_length - idx >= nee_length:
+        while idx <= hay_length - nee_length:
             if all(h == n for h, n in zip(haystack[idx:], needle)):
                 return idx
 
