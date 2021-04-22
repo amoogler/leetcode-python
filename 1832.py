3f6a7ca8cbfb80d95333a98d5c072129bfcc4e3d
Eric Li <lishuokobe2@gmail.com>
2021-04-21 22:25:10 -0700

    Improved 387 and 1832.

--- 1832.py
+++ 1832.py
@@ -1,12 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        alphebet = [False] * 26
-
-        for letter in sentence:
-            alphebet[ord(letter) - ord('a')] = True
-
-        for letter in alphebet:
-            if not letter:
-                return False
-
-        return True
+        return len(set(sentence)) == 26
--- 387.py
+++ 387.py
@@ -1,13 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        letter_idx = collections.defaultdict(list)
-        first_idx = len(s)
+        count = collections.Counter(s)
 
-        for i, letter in enumerate(s):
-            letter_idx[letter].append(i)
+        for i, char in enumerate(s):
+            if count[char] == 1:
+                return i
 
-        for value in letter_idx.values():
-            if len(value) == 1:
-                first_idx = min(value[0], first_idx)
-
-        return first_idx if first_idx != len(s) else -1
+        return -1
