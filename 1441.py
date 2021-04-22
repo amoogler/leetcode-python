75a5ed39172ead580d9da128767dd40286c625c0
Eric Li <lishuokobe2@gmail.com>
2021-04-22 08:28:01 -0700

    Improved 1441.py.

--- 1441.py
+++ 1441.py
@@ -1,11 +1,17 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         res = []
+        index = 0
 
-        for i in range(1, target[-1] + 1):
-            res.append("Push")
+        for i in range(1, n + 1):
+            if index == len(target):
+                break
 
-            if i not in target:
+            if target[index] == i:
+                res.append("Push")
+                index += 1
+            else:
+                res.append("Push")
                 res.append("Pop")
 
         return res
