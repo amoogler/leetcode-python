38cfceabc568504d371b893dec04bb65613a6e56
Eric Li <lishuokobe2@gmail.com>
2021-04-21 22:35:33 -0700

    Improved 155.py.

--- 155.py
+++ 155.py
@@ -1,31 +1,54 @@
+# class MinStack:
+
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.stack = []
+#         self.min_stack = []
+
+
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+
+#         if not self.min_stack or val <= self.min_stack[-1]:
+#             self.min_stack.append(val)
+
+
+#     def pop(self) -> None:
+#         if self.min_stack and self.stack[-1] == self.min_stack[-1]:
+#             self.min_stack.pop()
+
+#         self.stack.pop()
+
+#     def top(self) -> int:
+#         return self.stack[-1]
+
+#     def getMin(self) -> int:
+#         return self.min_stack[-1]
+
 class MinStack:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.stack = []
         self.min_stack = []
 
-
     def push(self, val: int) -> None:
-        self.stack.append(val)
-
-        if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)
-
+        if not self.min_stack or val <= self.min_stack[-1][1]:
+            self.min_stack.append((val, val))
+        else:
+            self.min_stack.append((val, self.min_stack[-1][1]))
 
     def pop(self) -> None:
-        if self.min_stack and self.stack[-1] == self.min_stack[-1]:
-            self.min_stack.pop()
-
-        self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.min_stack[-1][0]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.min_stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
