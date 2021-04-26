6f7f3b76894aa64f6d52d7acd47145c2163f09f9
Eric Li <lishuokobe2@gmail.com>
2021-04-25 17:00:23 -0700

    Check in 540.py.

--- 540.py
+++ 540.py
@@ -1,3 +1,23 @@
+# class Solution:
+#     def singleNonDuplicate(self, nums: List[int]) -> int:
+#         start, end = 0, len(nums) - 1
+
+#         while start < end:
+#             mid = start + (end - start) // 2
+
+#             if nums[mid - 1] != nums[mid] != nums[mid + 1]:
+#                 return nums[mid]
+#             elif nums[mid - 1] != nums[mid] and mid % 2 == 0: # mid is even, a,a,b,b,C,c
+#                 start = mid + 2
+#             elif nums[mid - 1] != nums[mid] and mid % 2 == 1: # mid is odd, a,a,b,b,c,D,d
+#                 end = mid - 1
+#             elif nums[mid] != nums[mid + 1] and mid % 2 == 0: # mid is even, a,a,b,c,C,d
+#                 end = mid - 2
+#             elif nums[mid] != nums[mid + 1] and mid % 2 == 1: # mid is odd, a,a,b,b,c,C,d
+#                 start = mid + 1
+
+#         return nums[start]
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         start, end = 0, len(nums) - 1
@@ -5,15 +25,15 @@ class Solution:
         while start < end:
             mid = start + (end - start) // 2
 
-            if nums[mid - 1] != nums[mid] != nums[mid + 1]:
-                return nums[mid]
-            elif nums[mid - 1] != nums[mid] and mid % 2 == 0: # mid is even, a,a,b,b,C,c
-                start = mid + 2
-            elif nums[mid - 1] != nums[mid] and mid % 2 == 1: # mid is odd, a,a,b,b,c,D,d
-                end = mid - 1
-            elif nums[mid] != nums[mid + 1] and mid % 2 == 0: # mid is even, a,a,b,c,C,d
-                end = mid - 2
-            elif nums[mid] != nums[mid + 1] and mid % 2 == 1: # mid is odd, a,a,b,b,c,C,d
-                start = mid + 1
+            if mid % 2 == 0:
+                if nums[mid] == nums[mid + 1]:
+                    start = mid + 2
+                else:
+                    end = mid
+            else:
+                if nums[mid] == nums[mid + 1]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
 
         return nums[start]
