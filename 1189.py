90e5d668d733c2331b08af96c7bc326979bca242
Eric Li <lishuokobe2@gmail.com>
2021-04-28 14:59:43 -0700

    Improved 1189.py.

--- 1189.py
+++ 1189.py
@@ -1,13 +1,19 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        def ok(letter_count: Counter) -> bool:
+            return letter_count['b'] >= 1 and \
+                   letter_count['a'] >= 1 and \
+                   letter_count['l'] >= 2 and \
+                   letter_count['o'] >= 2 and \
+                   letter_count['n'] >= 1
+
         letter_count = Counter(text)
         max_num = 0
 
-        while 1:
+        while ok(letter_count):
             for letter in "balloon":
-                if letter_count[letter] > 0:
-                    letter_count[letter] -= 1
-                else:
-                    return max_num
+                letter_count[letter] -= 1
 
             max_num += 1
+
+        return max_num
