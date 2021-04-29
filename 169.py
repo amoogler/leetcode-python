58c09279584f005dad2fce1f4f0f4ffc1659057a
Eric Li <lishuokobe2@gmail.com>
2021-04-29 09:31:11 -0700

    Improved 169.py.

--- 169.py
+++ 169.py
@@ -6,16 +6,17 @@
 # O(n) time complexity.
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = [nums[0], 1]
+        curr_majority = nums[0]
+        curr_count  = 1
 
         for num in nums[1:]:
-            if num == count[0]:
-                count[1] += 1
+            if num == curr_majority:
+                curr_count += 1
             else:
-                if count[1] == 0:
-                    count[0] = num
-                    count[1] = 1
+                if curr_count == 0:
+                    curr_majority = num
+                    curr_count = 1
                 else:
-                    count[1] -= 1
+                    curr_count -= 1
 
-        return count[0]
+        return curr_majority
