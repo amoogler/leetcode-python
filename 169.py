8439d671dba5112c79e2914f9388e4e3fc4e0256
Eric Li <lishuokobe2@gmail.com>
2021-04-28 20:27:43 -0700

    Improved 169.py.

--- 169.py
+++ 169.py
@@ -1,4 +1,21 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         sorted_nums = sorted(nums)
+#         return sorted_nums[len(nums) // 2]
+
+# O(n) time complexity.
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        sorted_nums = sorted(nums)
-        return sorted_nums[len(nums) // 2]
+        count = [nums[0], 1]
+
+        for num in nums[1:]:
+            if num == count[0]:
+                count[1] += 1
+            else:
+                if count[1] == 0:
+                    count[0] = num
+                    count[1] = 1
+                else:
+                    count[1] -= 1
+
+        return count[0]
