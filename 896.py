6f3ef285578269cd38748d2872f2b11c2ebdbb96
Eric Li <lishuokobe2@gmail.com>
2021-04-29 22:43:14 -0700

    Check in 896.py.

---
+++ 896.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        increase, decrease = True, True
+
+        for i in range(len(A) - 1):
+            if A[i] > A[i + 1]:
+                increase = False
+
+            if A[i] < A[i + 1]:
+                decrease = False
+
+        return increase or decrease
