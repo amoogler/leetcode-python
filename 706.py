3902edaa1483b1282d7486193a109416edfebc99
Eric Li <lishuokobe2@gmail.com>
2021-05-03 20:39:58 -0700

    Improved 706.py.

--- 706.py
+++ 706.py
@@ -4,27 +4,27 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        self.space_size = 9999
-        self.space = [[] for _ in range(self.space_size)]
+        self.SPACE_SIZE = 9999
+        self.space = [[] for _ in range(self.SPACE_SIZE)]
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        hashcode = key % self.space_size
+        hashcode = self.computeHash(key)
 
-        for idx, [k, v] in enumerate(self.space[hashcode]):
+        for idx, (k, v) in enumerate(self.space[hashcode]):
             if k == key:
-                self.space[hashcode][idx] = [key, value]
+                self.space[hashcode][idx] = (key, value)
                 return
 
-        self.space[hashcode].append([key, value])
+        self.space[hashcode].append((key, value))
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        hashcode = key % self.space_size
+        hashcode = self.computeHash(key)
 
         for k, v in self.space[hashcode]:
             if k == key:
@@ -36,11 +36,14 @@ class MyHashMap:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        hashcode = key % self.space_size
+        hashcode = self.computeHash(key)
 
         for k, v in self.space[hashcode]:
             if k == key:
-                self.space[hashcode].remove([k, v])
+                self.space[hashcode].remove((k, v))
+
+    def computeHash(self, key: int) -> int:
+        return key % self.SPACE_SIZE
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
