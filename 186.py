8f983d77cfed3aa1ebd86fb6ee5ab6b482c76838
Eric Li <lishuokobe2@gmail.com>
2021-05-07 18:02:52 -0700

    Check in 186.py.

---
+++ 186.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse(start: int, end: int) -> None:
+            while start < end:
+                s[start], s[end] = s[end], s[start]
+                start += 1
+                end -= 1
+
+        N = len(s)
+        reverse(0, N - 1)
+        start, end = 0, 0
+
+        while end < N:
+            if s[end] == ' ':
+                reverse(start, end - 1)
+                end += 1
+                start = end
+            elif end == N - 1:
+                reverse(start, end)
+                end += 1
+            else:
+                end += 1
+
+        return s
