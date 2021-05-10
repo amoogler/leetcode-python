4d45d7518c0ec79bf94deda8d6144a7c391a63a5
Eric Li <lishuokobe2@gmail.com>
2021-05-10 15:11:14 -0700

    Improved 543.py.

--- 543.py
+++ 543.py
@@ -4,28 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def diameterOfBinaryTree(self, root: TreeNode) -> int:
+#         def getMaxDepth(node: TreeNode) -> int:
+#             if not node:
+#                 return 0
+
+#             return max(getMaxDepth(node.left), getMaxDepth(node.right)) + 1
+
+#         stack = [root]
+#         diameter = 0
+
+#         while stack:
+#             node = stack.pop()
+#             diameter = max(getMaxDepth(node.left) + getMaxDepth(node.right), diameter)
+
+#             if node.left:
+#                 stack.append(node.left)
+
+#             if node.right:
+#                 stack.append(node.right)
+
+#         return diameter
+
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        def getMaxDepth(node: TreeNode) -> int:
+        def getLongestPath(node: TreeNode) -> int:
             if not node:
                 return 0
 
-            if not node.left and not node.right:
-                return 1
+            left_path = getLongestPath(node.left)
+            right_path = getLongestPath(node.right)
+            diameter[0] = max(diameter[0], left_path + right_path)
 
-            return max(getMaxDepth(node.left), getMaxDepth(node.right)) + 1
+            return max(left_path, right_path) + 1
 
-        stack = [root]
-        diameter = 0
+        diameter = [0]
+        getLongestPath(root)
 
-        while stack:
-            node = stack.pop()
-            diameter = max(getMaxDepth(node.left) + getMaxDepth(node.right), diameter)
+        return diameter[0]
 
-            if node.left:
-                stack.append(node.left)
-
-            if node.right:
-                stack.append(node.right)
-
-        return diameter
