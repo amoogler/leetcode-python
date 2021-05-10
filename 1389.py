afd283230318c24d9e815566705f24f95e15c398
Eric Li <lishuokobe2@gmail.com>
2021-05-10 16:12:13 -0700

    Improved 1389.py.

--- 1389.py
+++ 1389.py
@@ -1,12 +1,21 @@
+# class Solution:
+#     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+#         N = len(nums)
+#         res = [0] * N
+
+#         for i in range(N):
+#             for j in range(N - 1, index[i], -1):
+#                 res[j] = res[j - 1]
+
+#             res[index[i]] = nums[i]
+
+#         return res
+
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
-        N = len(nums)
-        res = [0] * N
+        res = []
 
-        for i in range(N):
-            for j in range(N - 1, index[i], -1):
-                res[j] = res[j - 1]
-
-            res[index[i]] = nums[i]
+        for num, idx in zip(nums, index):
+            res.insert(idx, num)
 
         return res
