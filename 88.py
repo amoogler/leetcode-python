86ae782f10a0e72f1fae9f741c7dea874c3294a2
Eric Li <lishuokobe2@gmail.com>
2021-05-11 15:57:43 -0700

    Improved 88.py.

--- 88.py
+++ 88.py
@@ -8,12 +8,12 @@ class Solution:
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
-                k -= 1
                 i -= 1
             else:
                 nums1[k] = nums2[j]
-                k -= 1
                 j -= 1
+
+            k -= 1
 
         while j >= 0:
             nums1[k] = nums2[j]
