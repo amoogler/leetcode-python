733f4ddf1ac870d40c6abffe2771f47628183620
Eric Li <lishuokobe2@gmail.com>
2021-05-10 20:52:35 -0700

    Improved 1021.py.

---
+++ 1021.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        res, count = [], 0
+
+        for c in S:
+            if c == '(' and count > 0:
+                res.append(c)
+            elif c == ')' and count > 1:
+                res.append(c)
+
+            count += 1 if c == '(' else -1
+
+        return ''.join(res)
