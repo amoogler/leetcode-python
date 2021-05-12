ee3bcb3ac5e168f6d8873e514484936b1e809290
Eric Li <lishuokobe2@gmail.com>
2021-05-11 19:33:09 -0700

    Check in 415.py.

--- 415.py
+++ 415.py
@@ -1,18 +1,29 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        res = []
+        res = collections.deque([])
         carry, p1, p2 = 0, len(num1) - 1, len(num2) - 1
 
-        while p1 >= 0 or p2 >= 0:
-            x1 = ord(num1[p1]) - ord('0') if p1 >= 0 else 0
-            x2 = ord(num2[p2]) - ord('0') if p2 >= 0 else 0
-            value = (x1 + x2 + carry) % 10
+        while p1 >= 0 and p2 >= 0:
+            x1 = ord(num1[p1]) - ord('0')
+            x2 = ord(num2[p2]) - ord('0')
+            res.appendleft(str((x1 + x2 + carry) % 10))
             carry = (x1 + x2 + carry) // 10
-            res.append(value)
             p1 -= 1
             p2 -= 1
 
-        if carry > 0:
-            res.append(carry)
+        while p1 >= 0:
+            x1 = ord(num1[p1]) - ord('0')
+            res.appendleft(str((x1 + carry) % 10))
+            carry = (x1 + carry) // 10
+            p1 -= 1
 
-        return ''.join(str(x) for x in res[::-1])
+        while p2 >= 0:
+            x2 = ord(num2[p2]) - ord('0')
+            res.appendleft(str((x2 + carry) % 10))
+            carry = (x2 + carry) // 10
+            p2 -= 1
+
+        if carry > 0:
+            res.appendleft(str(carry))
+
+        return ''.join(res)
