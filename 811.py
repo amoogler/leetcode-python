4fd1f798015c135bc7670e6a8367a3d661c29d3b
Eric Li <lishuokobe2@gmail.com>
2021-05-13 21:43:56 -0700

    Check in 811.py.

---
+++ 811.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        domain_count = collections.defaultdict(int)
+        res = []
+
+        for cpdomain in cpdomains:
+            parts = cpdomain.split()
+            count, domain = int(parts[0]), parts[1]
+
+            for i in range(len(domain) - 1, -1, -1):
+                if domain[i] == '.':
+                    domain_count[domain[i + 1 : len(domain)]] += count
+                elif i == 0:
+                    domain_count[domain] += count
+
+        for k, v in domain_count.items():
+            res.append(' '.join([str(v), k]))
+
+        return res
