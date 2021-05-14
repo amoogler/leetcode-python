4467703b787cb134ab5903978cfb9a458e8d0cd5
Eric Li <lishuokobe2@gmail.com>
2021-05-13 20:11:28 -0700

    Improved 1720.py.

--- 1720.py
+++ 1720.py
@@ -1,10 +1,8 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
         res = [first]
-        i = 0
 
         for num in encoded:
-            res.append(num ^ res[i])
-            i += 1
+            res.append(num ^ res[-1])
 
         return res
