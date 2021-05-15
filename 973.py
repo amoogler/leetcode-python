cee249d8ff7cae6c0752cc6a1bda0b87f9ec9a7a
Eric Li <lishuokobe2@gmail.com>
2021-05-14 20:48:43 -0700

    Improved 973.py.

--- 973.py
+++ 973.py
@@ -4,14 +4,13 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap, res = [], []
 
-        for point in points:
-            x, y = point[0], point[1]
+        for x, y in points:
             distance = pow(x, 2) + pow(y, 2)
-            heapq.heappush(heap, (distance, point))
+            heapq.heappush(heap, (distance, [x, y]))
 
         elements = heapq.nsmallest(k, heap)
 
-        for e in elements:
-            res.append(e[1])
+        for distance, point in elements:
+            res.append(point)
 
         return res
