a21130c1a78fade94319baa30463c5605938cdcc
Eric Li <lishuokobe2@gmail.com>
2021-05-23 14:20:47 -0700

    Improved 139.py.

--- 139.py
+++ 139.py
@@ -1,10 +1,23 @@
 # Recursion + Memoization
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         words = set(wordDict)
+
+#         @lru_cache
+#         def dfs(s: str):
+#             return s in words or any(s[:i] in words and dfs(s[i:]) for i in range(1, len(s)))
+
+#         return dfs(s)
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
 
-        @lru_cache
-        def dfs(s: str):
-            return s in words or any(s[:i] in words and dfs(s[i:]) for i in range(1, len(s)))
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
 
-        return dfs(s)
+        for i in range(1, len(s) + 1):
+            if any(dp[j] and s[j:i] in words for j in range(i)):
+                dp[i] = True
+
+        return dp[len(s)]
