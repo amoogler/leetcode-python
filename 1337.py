c9505c1075e4f8b7c5b22f8b3417b07358aaa5e4
Eric Li <lishuokobe2@gmail.com>
2021-05-30 13:44:59 -0700

    Improved 1337.py.

--- 1337.py
+++ 1337.py
@@ -1,13 +1,38 @@
+# class Solution:
+#     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+#         rows, res = [], []
+
+#         for i in range(len(mat)):
+#             rows.append((Counter(mat[i])[1], i))
+
+#         rows = sorted(rows)
+
+#         for row in rows:
+#             res.append(row[1])
+
+#         return res[:k]
+
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        rows, res = [], []
+        def binary_search(row: List[int]) -> int:
+            left, right = 0, len(row)
 
-        for i in range(len(mat)):
-            rows.append((Counter(mat[i])[1], i))
+            while left < right:
+                mid = left + (right - left) // 2
 
-        rows = sorted(rows, key=lambda x : x[0])
+                if row[mid] == 1:
+                    left = mid + 1
+                else:
+                    right = mid
 
-        for row in rows:
-            res.append(row[1])
+            return left
 
-        return res[:k]
+        res, heap = [], []
+
+        for idx, row in enumerate(mat):
+            heapq.heappush(heap, (binary_search(row), idx))
+
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+
+        return res
