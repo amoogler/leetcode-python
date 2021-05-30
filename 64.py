1816e3b2b2ae28b2acbbe4cd493994c4a029b6f6
Eric Li <lishuokobe2@gmail.com>
2021-05-30 10:08:58 -0700

    Improved 64.py.

--- 64.py
+++ 64.py
@@ -1,10 +1,10 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        dp = [[0] * n for _ in range(m)]
+        R, C = len(grid), len(grid[0])
+        dp = [[0] * C for _ in range(R)]
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(R):
+            for j in range(C):
                 if i == 0 and j == 0:
                     dp[i][j] = grid[i][j]
                 elif i == 0:
@@ -14,4 +14,4 @@ class Solution:
                 else:
                     dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
 
-        return dp[m - 1][n - 1]
+        return dp[R - 1][C - 1]
