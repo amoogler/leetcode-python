9a67afdb4f9cbcb912b94002ddcfa286cf129bd2
Eric Li <lishuokobe2@gmail.com>
2021-06-03 21:53:50 -0700

    Improved 621.py.

--- 621.py
+++ 621.py
@@ -1,20 +1,13 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         task_freqs = Counter(tasks)
-        sorted_freqs = sorted(task_freqs.values(), reverse=True)
+        max_freq = max(task_freqs.values())
+        max_count = list(task_freqs.values()).count(max_freq)
 
-        max_freq, max_count = sorted_freqs[0], 1
-
-        for freq in sorted_freqs[1:]:
-            if freq == max_freq:
-                max_count += 1
-            else:
-                break
-
-        part_count = max_freq - 1
-        part_length = n - (max_count - 1)
-        empty_slots = part_count * part_length
-        left_task_slots = len(tasks) - max_freq * max_count
-        idles = max(0, empty_slots - left_task_slots)
+        interval_count = max_freq - 1
+        interval_length = n - (max_count - 1)
+        empty_slots = interval_count * interval_length
+        left_tasks = len(tasks) - max_freq * max_count
+        idles = max(0, empty_slots - left_tasks)
 
         return len(tasks) + idles
