3434f37f3743e3aa4924e67aa86ea15f41cef4d2
Eric Li <lishuokobe2@gmail.com>
2021-06-07 20:36:03 -0700

    Check in BFS solution for 200.py.

--- 200.py
+++ 200.py
@@ -1,3 +1,4 @@
+# DFS
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
@@ -24,3 +25,37 @@ class Solution:
 
         for dr, dc in DIR:
             self.dfs(grid, row + dr, col + dc)
+
+# BFS
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        R, C = len(grid), len(grid[0])
+        count = 0
+
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == '1':
+                    grid[i][j] = '0'
+                    count += 1
+                    neighbors = deque([(i, j)])
+
+                    while neighbors:
+                        row, col = neighbors.popleft()
+
+                        if row - 1 >= 0 and grid[row - 1][col] == '1':
+                            neighbors.append((row - 1, col))
+                            grid[row - 1][col] = '0'
+
+                        if row + 1 < R and grid[row + 1][col] == '1':
+                            neighbors.append((row + 1, col))
+                            grid[row + 1][col] = '0'
+
+                        if col - 1 >= 0 and grid[row][col - 1] == '1':
+                            neighbors.append((row, col - 1))
+                            grid[row][col - 1] = '0'
+
+                        if col + 1 < C and grid[row][col + 1] == '1':
+                            neighbors.append((row, col + 1))
+                            grid[row][col + 1] = '0'
+
+        return count
