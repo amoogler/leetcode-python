805cc0cdc055c7da163e4e60727bbe1a2ff2418a
Eric Li <lishuokobe2@gmail.com>
2021-06-20 14:07:53 -0700

    Improve inorder traversal template.

--- 94.py
+++ 94.py
@@ -21,18 +21,35 @@
 #         self.inorder(node.right, inorder_nodes)
 
 # Iterative
+# class Solution:
+#     def inorderTraversal(self, root: TreeNode) -> List[int]:
+#         inorder_nodes, stack = [], []
+#         current = root
+
+#         while stack or current:
+#             if current:
+#                 stack.append(current)
+#                 current = current.left
+#             else:
+#                 node = stack.pop()
+#                 inorder_nodes.append(node.val)
+#                 current = node.right
+
+#         return inorder_nodes
+
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        inorder_nodes, stack = [], []
-        current = root
+        def pushleft(stack: List[int], node: TreeNode):
+            while node:
+                stack.append(node)
+                node = node.left
 
-        while stack or current:
-            if current:
-                stack.append(current)
-                current = current.left
-            else:
-                node = stack.pop()
-                inorder_nodes.append(node.val)
-                current = node.right
+        inorder_nodes, stack = [], []
+        pushleft(stack, root)
+
+        while stack:
+            node = stack.pop()
+            inorder_nodes.append(node.val)
+            pushleft(stack, node.right)
 
         return inorder_nodes
