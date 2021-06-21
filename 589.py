600b8b0079abb45631e67262b031b37f24c40f2c
Eric Li <lishuokobe2@gmail.com>
2021-06-20 20:50:03 -0700

    Improved 589.py.

--- 589.py
+++ 589.py
@@ -6,15 +6,30 @@ class Node:
         self.children = children
 """
 
+# class Solution:
+#     def preorder(self, root: 'Node') -> List[int]:
+#         preorder_nodes = []
+#         self.get_preorder(root, preorder_nodes)
+#         return preorder_nodes
+
+#     def get_preorder(self, node: 'Node', preorder_nodes: List[int]):
+#         if node:
+#             preorder_nodes.append(node.val)
+
+#             for child in node.children:
+#                 self.get_preorder(child, preorder_nodes)
+
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
-        preorder_nodes = []
-        self.get_preorder(root, preorder_nodes)
+        preorder_nodes, stack = [], [root]
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                preorder_nodes.append(node.val)
+
+                for i in range(len(node.children) - 1, -1, -1):
+                    stack.append(node.children[i])
+
         return preorder_nodes
-
-    def get_preorder(self, node: 'Node', preorder_nodes: List[int]):
-        if node:
-            preorder_nodes.append(node.val)
-
-            for child in node.children:
-                self.get_preorder(child, preorder_nodes)
