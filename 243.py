1e6c63f910400eef8b2a7b4c1e9022d2a697d666
Eric Li <lishuokobe2@gmail.com>
2021-08-22 11:42:19 -0700

    Refinee 243.py.

--- 243.py
+++ 243.py
@@ -1,16 +1,15 @@
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        index_1, index_2 = -1, -1
-        min_distance = len(wordsDict)
+        index_1, index_2, min_distance = -1, -1, len(wordsDict)
 
         for idx, word in enumerate(wordsDict):
             if word == word1:
                 index_1 = idx
-            elif word == word2:
+
+            if word == word2:
                 index_2 = idx
 
-            if (word == word1 or word == word2) and \
-               (index_1 != -1 and index_2 != -1):
+            if (word == word1 or word == word2) and (index_1 != -1 and index_2 != -1):
                 min_distance = min(min_distance, abs(index_1 - index_2))
 
         return min_distance
