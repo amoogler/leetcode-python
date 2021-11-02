29d6cc8c5829842eff69f71cd98a6a3a4a498396
Eric Li <lishuokobe2@gmail.com>
2021-11-01 20:33:37 -0700

    Update 110.py.

--- 110.py
+++ 110.py
@@ -4,18 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Top-Down Recursion
+# class Solution:
+#     def isBalanced(self, root: TreeNode) -> bool:
+#         def getTreeDepth(node: TreeNode) -> int:
+#             if not node:
+#                 return 0
+
+#             return max(getTreeDepth(node.left), getTreeDepth(node.right)) + 1
+
+#         if not root:
+#             return True
+
+#         if abs(getTreeDepth(root.left) - getTreeDepth(root.right)) > 1:
+#             return False
+
+#         return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+# Bottom-up Recursion
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        def getTreeDepth(node: TreeNode) -> int:
+        def helper(node: TreeNode) -> (bool, int):
             if not node:
-                return 0
+                return True, 0
 
-            return max(getTreeDepth(node.left), getTreeDepth(node.right)) + 1
+            left_ok, left_depth = helper(node.left)
+            right_ok, right_depth = helper(node.right)
 
-        if not root:
-            return True
+            if not left_ok or not right_ok:
+                return False, 0
 
-        if abs(getTreeDepth(root.left) - getTreeDepth(root.right)) > 1:
-            return False
+            return (abs(left_depth - right_depth) < 2), 1 + max(left_depth, right_depth)
 
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        return helper(root)[0]
