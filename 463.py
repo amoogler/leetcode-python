0014a03c5049e74a2a9eb04cedfdcf91ec069ee0
Eric Li <lishuokobe2@gmail.com>
2021-11-02 22:36:41 -0700

    Improved 463.py.

--- 463.py
+++ 463.py
@@ -1,21 +1,19 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        DIRS = ((1, 0), (-1, 0), (0, 1), (0, -1))
         R, C = len(grid), len(grid[0])
         perimeter = 0
 
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j] == 1:
-                    if i - 1 < 0 or grid[i - 1][j] == 0:
-                        perimeter += 1
+        for r, c in product(range(R), range(C)):
+            if grid[r][c] == 0:
+                continue
 
-                    if i + 1 >= R or grid[i + 1][j] == 0:
-                        perimeter += 1
+            for dr, dc in DIRS:
+                nx, ny = r + dr, c + dc
 
-                    if j - 1 < 0 or grid[i][j - 1] == 0:
-                        perimeter += 1
-
-                    if j + 1 >= C or grid[i][j + 1] == 0:
-                        perimeter += 1
+                if not (0 <= nx < R and 0 <= ny < C):
+                    perimeter += 1
+                elif grid[nx][ny] == 0:
+                    perimeter += 1
 
         return perimeter
