5683244ec2faaa25ed40a9fc79fc3fba2ad9dbc0
Eric Li <lishuokobe2@gmail.com>
2021-11-07 11:52:46 -0800

    New version of 36.py.

--- 36.py
+++ 36.py
@@ -1,29 +1,44 @@
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+#         N = 9
+
+#         rows = [set() for _ in range(N)]
+#         cols = [set() for _ in range(N)]
+#         boxes = [set() for _ in range(N)]
+
+#         for r in range(N):
+#             for c in range(N):
+#                 value = board[r][c]
+
+#                 if value == '.':
+#                     continue
+
+#                 if value in rows[r]:
+#                     return False
+#                 rows[r].add(value)
+
+#                 if value in cols[c]:
+#                     return False
+#                 cols[c].add(value)
+
+#                 idx = (r // 3) * 3 + c // 3
+#                 if value in boxes[idx]:
+#                     return False
+#                 boxes[idx].add(value)
+
+#         return True
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        N = 9
+        seen = []
 
-        rows = [set() for _ in range(N)]
-        cols = [set() for _ in range(N)]
-        boxes = [set() for _ in range(N)]
-
-        for r in range(N):
-            for c in range(N):
-                value = board[r][c]
-
-                if value == '.':
+        for r, row in enumerate(board):
+            for c, char in enumerate(row):
+                if char == '.':
                     continue
 
-                if value in rows[r]:
-                    return False
-                rows[r].add(value)
+                seen.append((r, char))
+                seen.append((char, c))
+                seen.append((r // 3, c // 3, char))
 
-                if value in cols[c]:
-                    return False
-                cols[c].add(value)
-
-                idx = (r // 3) * 3 + c // 3
-                if value in boxes[idx]:
-                    return False
-                boxes[idx].add(value)
-
-        return True
+        return len(seen) == len(set(seen))
