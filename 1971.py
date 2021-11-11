ded6e82756021c4e7df6ab5298f8fdc4ba8b94b4
Eric Li <lishuokobe2@gmail.com>
2021-11-10 21:15:24 -0800

    Add DFS solution for 1971.py.

--- 1971.py
+++ 1971.py
@@ -1,3 +1,27 @@
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+#         seen, graph = set(), defaultdict(list)
+
+#         for s, e in edges:
+#             graph[s].append(e)
+#             graph[e].append(s)
+
+#         queue = deque([start])
+#         seen.add(start)
+
+#         while queue:
+#             curr = queue.popleft()
+
+#             if curr == end:
+#                 return True
+
+#             for v in graph[curr]:
+#                 if v not in seen:
+#                     queue.append(v)
+#                     seen.add(v)
+
+#         return False
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
         seen, graph = set(), defaultdict(list)
@@ -6,18 +30,19 @@ class Solution:
             graph[s].append(e)
             graph[e].append(s)
 
-        queue = deque([start])
-        seen.add(start)
-
-        while queue:
-            curr = queue.pop()
-
-            if curr == end:
+        def dfs(node, end, seen):
+            if node == end:
                 return True
 
-            for v in graph[curr]:
-                if v not in seen:
-                    queue.append(v)
-                    seen.add(v)
+            if node in seen:
+                return False
 
-        return False
+            seen.add(node)
+
+            for v in graph[node]:
+                if dfs(v, end, seen):
+                    return True
+
+            return False
+
+        return dfs(start, end, seen)
