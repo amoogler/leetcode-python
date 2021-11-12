5ae0b7aaedf80696f42d619fa5e4728f42f75ee2
Eric Li <lishuokobe2@gmail.com>
2021-11-12 14:23:20 -0800

    Improved 841.py.

--- 841.py
+++ 841.py
@@ -1,38 +1,32 @@
+# DFS Solution
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def dfs(room: int):
+            if room not in visited:
+                visited.add(room)
+
+                for key in rooms[room]:
+                    dfs(key)
+
+        visited = set()
+        dfs(0)
+        return len(visited) == len(rooms)
+
+# BFS Solution.
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visited = set()
+        queue = deque([0])
 
-        def dfs(room: int):
-            if room in visited:
-                return False
-
+        while queue:
+            room = queue.popleft()
             visited.add(room)
 
             if len(visited) == len(rooms):
                 return True
 
             for key in rooms[room]:
-                if dfs(key):
-                    return True
+                if key not in visited:
+                    queue.append(key)
 
-            return False
-
-        return dfs(0)
-
-# class Solution:
-#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-#         visited = set()
-#         queue = deque([0])
-
-#         while queue:
-#             room = queue.popleft()
-#             visited.add(room)
-
-#             if len(visited) == len(rooms):
-#                 return True
-
-#             for key in rooms[room]:
-#                 if key not in visited:
-#                     queue.append(key)
-
-#         return False
+        return False
