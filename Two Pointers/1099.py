25525cbdf7b95b43a1bdd54b4f53e6ba52a7300e
Eric Li <lishuokobe2@gmail.com>
2021-11-17 14:06:59 -0800

    Check in 1099.py.

---
+++ Two Pointers/1099.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        sorted_list = sorted(nums)
+        left, right = 0, len(sorted_list) - 1
+        res = -1
+
+        while left < right:
+            nums_sum = sorted_list[left] + sorted_list[right]
+            if nums_sum < k:
+                res = max(res, nums_sum)
+                left += 1
+            else:
+                right -= 1
+
+        return res
