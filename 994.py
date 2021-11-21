e7d2ff97cfcf2ea0b2b9ae940496931bd480586f
Eric Li <lishuokobe2@gmail.com>
2021-11-21 13:31:33 -0800

    Improved 994.py.

--- 994.py
+++ 994.py
@@ -5,12 +5,11 @@ class Solution:
         fresh_oranges = 0
 
         # Find all the rotten oranges.
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                elif grid[i][j] == 1:
-                    fresh_oranges += 1
+        for i, j in product(range(R), range(C)):
+            if grid[i][j] == 2:
+                queue.append((i, j))
+            elif grid[i][j] == 1:
+                fresh_oranges += 1
 
         if fresh_oranges == 0:
             return 0
@@ -25,15 +24,12 @@ class Solution:
                 r, c = queue.popleft()
 
                 for dr, dc in DIRS:
-                    new_r, new_c = r + dr, c + dc
+                    nr, nc = r + dr, c + dc
 
-                    if new_r < 0 or new_r >= R or new_c < 0 or new_c >= C or \
-                        grid[new_r][new_c] != 1:
-                        continue
-
-                    grid[new_r][new_c] = 2
-                    fresh_oranges -= 1
-                    queue.append((new_r, new_c))
+                    if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh_oranges -= 1
+                        queue.append((nr, nc))
 
             minute += 1
 
