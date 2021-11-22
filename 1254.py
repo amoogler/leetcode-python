78ac6cc8a55dd31f1e0150e29dea47597ad91496
Eric Li <lishuokobe2@gmail.com>
2021-11-21 16:46:24 -0800

    Improved 1254.py.

--- 1254.py
+++ 1254.py
@@ -1,27 +1,34 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        count, row, col = 0, len(grid), len(grid[0])
+        self.R, self.C = len(grid), len(grid[0])
+        self.DIRS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        self.grid = grid
+        borders, islands = deque([]), deque([])
+        count = 0
 
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == 0 and self.isClosedIsland(grid, i, j):
-                    count += 1
+        for i, j in product(range(self.R), range(self.C)):
+            if (i == 0 or i == self.R - 1 or j == 0 or j == self.C - 1) and grid[i][j] == 0:
+                borders.append((i, j))
+
+        # Mark escaped '0' as '1'.
+        self.bfs(borders)
+
+        # Now, question becomes how many islands in the grid.
+        for i, j in product(range(1, self.R - 1), range(1, self.C - 1)):
+            if grid[i][j] == 0:
+                islands.append((i, j))
+                self.bfs(islands)
+                count += 1
+
         return count
 
-    def isClosedIsland(self, grid: List[List[int]], i: int, j: int) -> bool:
-        is_closed, row, col = True, len(grid), len(grid[0])
+    def bfs(self, queue: deque):
+        while queue:
+            r, c  = queue.popleft()
+            self.grid[r][c] = 1
 
-        if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] != 0:
-            return is_closed
-        elif grid[i][j] == 0:
-            grid[i][j] = 1
+            for dr, dc in self.DIRS:
+                nr, nc = r + dr, c + dc
 
-            border = i == 0 or i == row - 1 or j == 0 or j == col - 1
-            is_closed &= not border
-
-            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-            for a, b in directions:
-                is_closed &= self.isClosedIsland(grid, i + a, j + b)
-
-        return is_closed
+                if 0 <= nr < self.R and 0 <= nc < self.C and self.grid[nr][nc] == 0:
+                    queue.append((nr, nc))
