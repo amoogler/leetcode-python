2607c8c1d3234c08091d027ea51fb7765a1704fb
Eric Li <lishuokobe2@gmail.com>
2021-11-22 11:39:53 -0800

    Improved 547.py.

--- 547.py
+++ 547.py
@@ -1,46 +1,52 @@
 # This problem is to find number of connected component in an un-directed graph.
+# This problem is a graph version of LC-200.
 
 # BFS Iterative Solution
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        N = len(isConnected)
-        visited = [0] * N
-        queue = deque()
+        self.N = len(isConnected)
+        seen = set()
         count = 0
 
-        for i in range(N):
-            if not visited[i]:
-                queue.append(i)
-
-                while queue:
-                    node = queue.popleft()
-                    visited[node] = 1
-
-                    for j in range(N):
-                        if isConnected[node][j] == 1 and visited[j] == 0:
-                            queue.append(j)
-
+        for i in range(self.N):
+            if i not in seen:
                 count += 1
+                seen.add(0)
+                self.bfs(seen, i, isConnected)
 
         return count
+
+    def bfs(self, seen: set, pos: int, isConnected: List[List[int]]) -> None:
+        queue = deque([pos])
+
+        while queue:
+            node = queue.popleft()
+
+            for j in range(self.N):
+                if isConnected[node][j] == 1 and j not in seen:
+                    queue.append(j)
+                    seen.add(j)
 
 # DFS Recursive Solution
 class Solution:
-    def dfs(self, isConnected: List[List[int]], visited: List[int], i: int) -> None:
-        for j in range(len(isConnected)):
-            if isConnected[i][j] == 1 and visited[j] == 0:
-                visited[j] = 1
-                self.dfs(isConnected, visited, j)
-
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        N = len(isConnected)
-        visited = [0] * N
+        self.N = len(isConnected)
+        seen = set()
         count = 0
 
-        for i in range(N):
-            if not visited[i]:
-                visited[i] = 1
-                self.dfs(isConnected, visited, i)
+        for i in range(self.N):
+            if i not in seen:
                 count += 1
+                self.dfs(seen, i, isConnected)
 
         return count
+
+    def dfs(self, seen: set, pos: int, isConnected: List[List[int]]) -> None:
+        if pos in seen:
+            return
+
+        seen.add(pos)
+
+        for j in range(self.N):
+            if isConnected[pos][j] == 1:
+                self.dfs(seen, j, isConnected)
