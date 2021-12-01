b7b23d6099664185e4b4cffb545fd48bc9c8e6eb
Eric Li <lishuokobe2@gmail.com>
2021-11-30 22:45:41 -0800

    Moved 1656 and 708.

--- Linked List/708-Insert_Into_A_Sorted_Circular_Linked_List.py
+++ Linked List/708-Insert_Into_A_Sorted_Circular_Linked_List.py
@@ -17,9 +17,11 @@ class Solution:
         prev, curr = head, head.next
 
         while prev.next != head:
+            # Case1: 1 <- Node(2) <- 3
             if prev.val <= node.val <= curr.val:
                 break
 
+            # Case2: 3 -> 1, 3 -> Node(4) -> 1, 3 -> Node(0) -> 1
             if prev.val > curr.val and (node.val > prev.val or node.val < curr.val):
                 break
 
