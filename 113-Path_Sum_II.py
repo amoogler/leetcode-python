9bf974e6a3992cb51795f0621003cff404c14d0a
Eric Li <lishuokobe2@gmail.com>
2021-12-23 22:13:11 -0800

    Refined 113.py.

--- 113-Path_Sum_II.py
+++ 113-Path_Sum_II.py
@@ -7,17 +7,20 @@
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
         paths, curr_path = [], []
-        self.dfs(root, targetSum, curr_path, paths)
+        self.backtrack(root, targetSum, curr_path, paths)
         return paths
 
-    def dfs(self, node: TreeNode, targetSum: int, curr_path: List[int], paths: List[List[int]]):
+    def backtrack(self, node: TreeNode, targetSum: int, curr_path: List[int], paths: List[List[int]]):
         if not node:
             return
 
         if not node.left and not node.right and node.val == targetSum:
             curr_path.append(node.val)
-            paths.append(curr_path)
+            paths.append(curr_path[:])
+            del curr_path[-1]
             return
 
-        self.dfs(node.left, targetSum - node.val, curr_path + [node.val], paths)
-        self.dfs(node.right, targetSum - node.val, curr_path + [node.val], paths)
+        curr_path.append(node.val)
+        self.backtrack(node.left, targetSum - node.val, curr_path, paths)
+        self.backtrack(node.right, targetSum - node.val, curr_path, paths)
+        del curr_path[-1]
