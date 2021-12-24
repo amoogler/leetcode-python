e3f7c929e3e7cf13c5a905c454c943fa63703ecf
Eric Li <lishuokobe2@gmail.com>
2021-12-23 23:07:41 -0800

    Refined 104.py and 559.py.

---
+++ Tree/BFS-DFS/104-Maximum_Depth_Of_Binary_Tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS recursive solution.
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+
+        if root.left == None and root.right == None:
+            return 1
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+# BFS solution.
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        depth = 0
+        queue = deque([root])
+
+        while queue:
+            queue_length = len(queue)
+
+            for _ in range(queue_length):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            depth += 1
+
+        return depth
--- Tree/BFS-DFS/559-Maximum_Depth_Of_N-ary_Tree.py
+++ Tree/BFS-DFS/559-Maximum_Depth_Of_N-ary_Tree.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 """
 # Definition for a Node.
 class Node:
@@ -7,26 +5,44 @@ class Node:
         self.val = val
         self.children = children
 """
+# DFS recursive solution.
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
 
+        if all(not child for child in root.children):
+            return 1
+
+        max_depth = 0
+
+        for child in root.children:
+            max_depth = max(max_depth, self.maxDepth(child))
+
+        return max_depth + 1
+
+
+# BFS solution.
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
         max_depth = 0
-        
+
         if not root:
             return max_depth
-        
+
         queue = deque([root])
-        
+
         while queue:
             queue_length = len(queue)
-            
+
             for _ in range(queue_length):
                 node = queue.popleft()
-                
+
                 for child in node.children:
                     if child:
                         queue.append(child)
-                  
+
             max_depth += 1
-        
+
         return max_depth
+
