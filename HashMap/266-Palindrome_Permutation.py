bb33253368f3e9c083bb7d01b9b770130c057daa
Eric Li <lishuokobe2@gmail.com>
2021-12-27 21:09:52 -0800

    Check in 266.py.

---
+++ HashMap/266-Palindrome_Permutation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        count = Counter(list(s))
+        odd_number = 0
+
+        for frequency in count.values():
+            if frequency % 2 == 1:
+                odd_number += 1
+
+            if odd_number > 1:
+                return False
+
+        return True
