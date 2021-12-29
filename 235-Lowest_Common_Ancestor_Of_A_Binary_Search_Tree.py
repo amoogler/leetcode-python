726080700de06a822e2b3efc3b808794f7b29107
Eric Li <lishuokobe2@gmail.com>
2021-12-28 17:49:46 -0800

    Check in 235.py.

--- 235-Lowest_Common_Ancestor_Of_A_Binary_Search_Tree.py
+++ 235-Lowest_Common_Ancestor_Of_A_Binary_Search_Tree.py
@@ -5,14 +5,14 @@
 #         self.left = None
 #         self.right = None
 
-# class Solution:
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         if p.val > root.val and q.val > root.val:
-#             return self.lowestCommonAncestor(root.right, p, q)
-#         elif p.val < root.val and q.val < root.val:
-#             return self.lowestCommonAncestor(root.left, p, q)
-#         else:
-#             return root
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
