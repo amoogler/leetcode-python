4d924db0ce01b3f2bde14678dee2f541fc7bfae9
Eric Li <lishuokobe2@gmail.com>
2021-12-30 22:51:17 -0800

    Check in 865.py.

---
+++ Tree/DFS/Post-order/865-Smallest_Subtree_With_All_The_Deepest_Nodes.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        def deep(root):
+            if not root:
+                return 0, None
+
+            left, right = deep(root.left), deep(root.right)
+
+            if left[0] > right[0]:
+                return left[0] + 1, left[1]
+            elif left[0] < right[0]:
+                return right[0] + 1, right[1]
+            else:
+                return left[0] + 1, root
+
+        return deep(root)[1]
