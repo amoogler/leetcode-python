0ffe3b096df093c697ab14a7b58c8cdab0181336
Eric Li <lishuokobe2@gmail.com>
2022-01-02 17:17:40 -0800

    Check in 684.py.

---
+++ Graph/Union Find (Disjoint Set)/684-Redundant_Connection.py
@@ -0,0 +1,42 @@
+# Union Find Solution.
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
+
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] > self.rank[root_y]:
+            self.root[root_y] = root_x
+        elif self.rank[root_x] < self.rank[root_y]:
+            self.root[root_x] = root_y
+        else:
+            self.root[root_y] = root_x
+            self.rank[root_x] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges) + 1)
+        res = []
+
+        for x, y in edges:
+            if uf.connected(x, y):
+                res = [x, y]
+                continue
+
+            uf.union(x, y)
+
+        return res
