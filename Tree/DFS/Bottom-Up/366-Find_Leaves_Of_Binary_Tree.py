905989fb7347eedf82f152eae388437498faf227
Eric Li <lishuokobe2@gmail.com>
2022-01-06 12:47:53 -0800

    Check in 366.py.

---
+++ Tree/DFS/Bottom-Up/366-Find_Leaves_Of_Binary_Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Bottom-Up Approach.
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        def dfs(node: 'TreeNode') -> int:
+            if not node:
+                return -1
+
+            curr_height = 1 + max(dfs(node.left), dfs(node.right))
+
+            if curr_height == len(res):
+                res.append([])
+
+            res[curr_height].append(node.val)
+            node.left, node.right = None, None
+            return curr_height
+
+        dfs(root)
+
+        return res
