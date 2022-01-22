c20e5a18cb1571ade3f4f1c33bc02a79b9256da2
Eric Li <lishuokobe2@gmail.com>
2022-01-21 19:52:46 -0800

    Check in 989.py.

---
+++ Math/989-Add_To_Array-Form_Of_Integer.py
@@ -0,0 +1,7 @@
+# Treat k as the carry.
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        for i in range(len(num) - 1, -1, -1):
+            k, num[i] = divmod(num[i] + k, 10)
+
+        return [int(i) for i in str(k)] + num if k else num
