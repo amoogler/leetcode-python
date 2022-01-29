aa7fc27f9a0f47b242197afa79b7bfec8b33755b
Eric Li <lishuokobe2@gmail.com>
2022-01-29 13:37:06 -0800

    Check in 347.py and 692.py.

---
+++ Priority Queue/692-Top_K_Frequent_Words.py
@@ -0,0 +1,4 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = Counter(words)
+        return heapq.nsmallest(k, count, key=lambda word: (-count[word], word))
