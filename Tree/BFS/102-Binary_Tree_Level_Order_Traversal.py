e30a57ee77211a0df8e397b3cdb000aa32ff7eec
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-02 23:40:08 -0800

    Reformat 102.py.

--- Tree/BFS/102-Binary_Tree_Level_Order_Traversal.py
+++ Tree/BFS/102-Binary_Tree_Level_Order_Traversal.py
@@ -1,5 +1,3 @@
-from typing import List
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,28 +5,22 @@ from typing import List
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         levels = []
-        
         if not root:
             return levels
-
-        queue = collections.deque([root])
         
+        queue = collections.deque([root])
         while queue:
             level = []
             level_length = len(queue)
-            
             for _ in range(level_length):
                 node = queue.popleft()
                 level.append(node.val)
-            
                 if node.left:
                     queue.append(node.left)
-
                 if node.right:
                     queue.append(node.right)
-
             levels.append(level)
 
         return levels
