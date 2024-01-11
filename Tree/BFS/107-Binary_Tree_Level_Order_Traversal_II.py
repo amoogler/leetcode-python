d7f53c4ef2101a01cfde3ce9b614e9ed03fb8089
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-10 23:24:12 -0800

    Check in 107.py

--- Tree/BFS/107-Binary_Tree_Level_Order_Traversal_II.py
+++ Tree/BFS/107-Binary_Tree_Level_Order_Traversal_II.py
@@ -9,28 +9,22 @@ from collections import deque
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
         levels = []
-
         if not root:
             return levels
-        
+
         queue = deque([root])
-        
         while queue:
             level = []
-            level_length = len(queue)
-            
-            for _ in range(level_length):
+            queue_length = len(queue)
+            for _ in range(queue_length):
                 node = queue.popleft()
                 level.append(node.val)
-                
                 if node.left:
                     queue.append(node.left)
-                
                 if node.right:
                     queue.append(node.right)
-            
             levels.append(level)
-        
-        return reversed(levels)
+
+        return levels[::-1]
