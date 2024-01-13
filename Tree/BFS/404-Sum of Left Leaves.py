9ca103f0a198e62c2beb7f4c000c790ef4f51fad
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-13 14:20:43 -0800

    Move 404.py

--- Tree/BFS/404-Sum of Left Leaves.py
+++ Tree/BFS/404-Sum of Left Leaves.py
@@ -5,23 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         sum_left = 0
-
         if not root:
             return sum_left
-
-        queue = collections.deque([root])
-
+        
+        queue = deque([root])
         while queue:
             node = queue.popleft()
 
             if node.left:
                 queue.append(node.left)
-
                 if not node.left.left and not node.left.right:
                     sum_left += node.left.val
-
             if node.right:
                 queue.append(node.right)
 
