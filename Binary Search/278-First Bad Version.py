f540908a6e1c53b6f857e6a4c7b82e6f271cc0de
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-14 22:32:19 -0800

    Check in 897.py

--- Binary Search/278-First Bad Version.py
+++ Binary Search/278-First Bad Version.py
@@ -4,20 +4,13 @@
 # def isBadVersion(version):
 
 class Solution:
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        left = 1
-        right = n
-        
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
         while left < right:
             mid = (left + right) // 2
-            
             if isBadVersion(mid):
                 right = mid
             else:
                 left = mid + 1
-                
+        
         return left
