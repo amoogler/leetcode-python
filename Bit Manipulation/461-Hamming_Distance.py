80a1586be309a01c6e39c8632f8d02abbf646fd6
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-16 14:47:21 -0800

    Check in 429.py

--- Bit Manipulation/461-Hamming_Distance.py
+++ Bit Manipulation/461-Hamming_Distance.py
@@ -1,14 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        distance = 0       
-        num = x ^ y
-        
-        while num > 0:
-            bit = num & 1
-            
-            if bit == 1:
+        distance = 0
+        xor = x ^ y
+        while xor:
+            if xor & 1 == 1:
                 distance += 1
-            
-            num >>= 1
-            
+            xor >>= 1
+
         return distance
