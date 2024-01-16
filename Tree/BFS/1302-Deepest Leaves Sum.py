89f81888153ee122ae8e68edc909e111a59437c0
Eric L <80171898+amoogler@users.noreply.github.com>
2024-01-16 15:04:49 -0800

    Check in 1302.py

---
+++ Tree/BFS/1302-Deepest Leaves Sum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        queue = deque([root])
+
+        while queue:
+            queue_length = len(queue)
+            level_sum = 0
+            for _ in range(queue_length):
+                node = queue.popleft()
+                level_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+                res = level_sum
+        
+        return res
